c0544771339373808040c392644a48ef7d267796
HuangXinyue1017 <77265968+HuangXinyue1017@users.noreply.github.com>
2021-03-10 10:29:04 +0800

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,19 @@
+#The first number
+#The second number
+#Print the first two numbers
+
+a=1
+b=1
+print(a)
+print(b)
+
+#How many numbers have been printed
+n=2
+
+#Start
+while(n<13):
+	c=a+b
+	print(c)
+	a=b
+	b=c
+	n+=1
---
+++ r_rate.py
@@ -0,0 +1,12 @@
+#The number of IBI1 students
+#The infection rate
+#The time
+n=84
+r=1.1
+t=0
+
+#Start
+while(t!=5):
+	n*=r
+	print(n)
+	t+=1
